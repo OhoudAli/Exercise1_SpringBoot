615bdb1463f382fcd10ad83b968c6ed76ff7b537
OhoudAli <124017739+OhoudAli@users.noreply.github.com>
2025-03-04 13:36:12 +0300

    Add files via upload

---
+++ welcomeController/src/main/java/com/example/welcomecontroller/exersice1_springboot.java
@@ -0,0 +1,13 @@
+package com.example.welcomecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class exersice1_springboot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(exersice1_springboot.class, args);
+    }
+
+}
